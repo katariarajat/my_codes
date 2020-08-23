956e45fe23d682bd411625c02675d1d0b79a5e37
katariarajat <katariarajat34@gmail.com>
2020-08-23 13:06:52 +0530

    MODIFIED BINARY SEARCH

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -22,36 +22,90 @@
 #define ss second
 #define pb push_back
 #define pp pop_back
-
 using namespace std;
+
+int less_than_equal(vector<int> &a,int &n,int &x)
+{
+	int l=-1,r=n;
+	while(r>l+1)
+	{
+		int m = (l+r)/2;
+		if(a[m] > x)
+		{
+			r=m;
+		}
+		else 
+		{
+			l=m;
+		}
+	}
+	return l;
+}
+
+int greater_than_equal(vector<int> &a,int &n,int &x)
+{
+	int l=-1,r=n;
+	while(r>l+1)
+	{
+		int m = (l+r)/2;
+		if(a[m] < x)
+		{
+			l=m;
+		}
+		else
+		{
+			r=m;
+		}
+	}
+	return r;
+}
+
+int if_equal(vector<int> &a,int &n,int &x)
+{
+	int l=0,r=n-1,ok=0;
+	while(r>l)
+	{
+		int m = (l+r)/2;
+		if(a[m] > x)
+		{
+			l=m;
+		}
+		else if(a[m] < x)
+		{
+			r=m;
+		}
+		else
+		{
+			ok=1;
+			break;
+		}
+	}
+	return ok;
+}
+
 int main()
 {
 	int n,k;
 	cin>>n>>k;
-	int a[n];
-	for(int i=0;i<n;i++)
+	vector<int> a(n);
+	for (size_t i = 0; i < n; i++)
+	{
 		cin>>a[i];
-	int x;
-
+	}
+	int index;
 	for(int i=0;i<k;i++)
 	{
+		int x;
 		cin>>x;
-		int max=n-1,min=0,mid;
-		while(max>=min)
+		index=less_than_equal(a,n,x);
+		if(index==-1)
 		{
-			mid=(max+min)/2;
-			if(a[mid]>x)
-				max=mid-1;
-			else if(a[mid]<x)
-				min=mid+1;
-			else 
-				break;
+			cout<<"0"<<endl;
 		}
-		// cout<<mid<<a[mid]<<endl;
-		if(a[mid]==x)
-			cout<<"YES"<<endl;
-		else 
-			cout<<"NO"<<endl;
+		else
+		{
+			cout<<index+1<<endl;
+		}
 	}
 	return 0;
 }
